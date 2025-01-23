56518cea4086fb8ae287054d842d67f8e9e6cda1
Diya Jain <diyajain@usf.edu>
2025-01-23 14:36:25 -0600

    [any message]

--- app/main.py
+++ app/main.py
@@ -4,12 +4,12 @@ import sys
 
 def main():
     while True:  # Use a loop to keep the program running until 'exit 0' is entered
-        command = input("$ echo ")
+        command = input("$ ")
         if command == "exit 0":
             return 0  # Exit the program
 
         #print(f"{command}: command not found")
-        print(f"{command}")
+        print(f"echo {command}")
 
 if __name__ == "__main__":
     main()
