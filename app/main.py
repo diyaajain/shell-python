e2fb448ca314a51a4cb61ca525b3852b0eb8a825
Diya Jain <diyajain@usf.edu>
2025-01-23 14:26:21 -0600

    [any message]

--- app/main.py
+++ app/main.py
@@ -1,15 +1,14 @@
 import sys
 
-
 def main():
     # Uncomment this block to pass the first stage
     sys.stdout.write("$ ")
 
     # Wait for user input
-    
-    command = input()
-    print(f"{command}: command not found")
-    main()
+    while input == 0:
+        command = input()
+        print(f"{command}: command not found")
+        main()
 
 
 if __name__ == "__main__":
